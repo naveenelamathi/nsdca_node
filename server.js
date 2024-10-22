8de614803ab3d6ea91672949913469da1b3d6434
natarajan <naveenelamathi2018@gmail.com>
2024-10-22 16:57:49 +0530

    urls

--- server.js
+++ server.js
@@ -31,12 +31,8 @@ app.get('*', (req, res) => {
   });
 })
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-// const PORT = process.env.PORT || 3000;
-app.listen( () => {
-  console.log(`Server is running on port ${"https://nsdca-front-end-1.onrender.com"}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
