028ca737626bf11cd8888e2994937815df743ed1
natarajan <naveenelamathi2018@gmail.com>
2024-11-05 17:44:03 +0530

    hello

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 //   },
 // })
 // const { Pool } = require('pg');
-const PORT = process.env.PORT || 10000;
+// const PORT = process.env.PORT || 10000;
 const db = require('./db');
 app.use(bodyParser.json()); 
 const authController = require('./controllers/Signup.controller');
@@ -24,11 +24,11 @@ const verifyController = require('./controllers/Online.controller');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors({
-  origin: 'https://nsdca-front-end.onrender.com', 
-}));
+// app.use(cors({
+//   origin: 'https://nsdca-front-end.onrender.com', 
+// }));
 
 
 app.get('/', (req, res) => {
@@ -42,11 +42,11 @@ app.post('/verify',verifyController.verify);
 app.post('/verification',verifyController.verification);
 
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen( () => {
+  console.log(`Server is running on port `);
 });
 
