20b0b993002f8978919a69e884261e082eeb238e
natarajan <naveenelamathi2018@gmail.com>
2024-11-05 17:35:51 +0530

    ok

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
+// const { Sequelize, DataTypes } = require('sequelize');
+// const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
 
-const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  dialect: config.dialect,
-});
+// const sequelize = new Sequelize({
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   host: config.host,
+//   dialect: config.dialect,
+// });
 
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+// const db = {};
+// db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
 
 // Import models
 // db.Property = require('./models/property')(sequelize, DataTypes);
 
-module.exports = db;
+// module.exports = db;
