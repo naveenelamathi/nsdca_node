f2dd869b0eba4258fb13b2b303845ee326af0eb7
natarajan <naveenelamathi2018@gmail.com>
2024-11-18 15:17:03 +0530

    3

--- db.js
+++ db.js
@@ -1,19 +1,25 @@
-// const { Sequelize, DataTypes } = require('sequelize');
-// const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
+const { Sequelize, DataTypes } = require('sequelize');
+const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
 
-// const sequelize = new Sequelize({
-//   username: config.username,
-//   password: config.password,
-//   database: config.database,
-//   host: config.host,
-//   dialect: config.dialect,
-// });
+const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  dialect: config.dialect,
+});
 
-// const db = {};
-// db.Sequelize = Sequelize;
-// db.sequelize = sequelize;
+const db = {};
 
-
+// Import models manually
 // db.Property = require('./models/property')(sequelize, DataTypes);
+// db.User = require('./models/user')(sequelize, DataTypes);  // Example of another model
 
-// module.exports = db;
+// Add other models as needed
+// db.OtherModel = require('./models/otherModel')(sequelize, DataTypes);
+
+// Export db object with models
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+module.exports = db;
